c2ab2d25047027bec7e79e9ff6e057eca2e7a6e5
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-10-05 12:50:26 -0500

    CG 13-2

--- HighSchool/Program.cs
+++ HighSchool/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HighSchool
 {
@@ -6,7 +8,34 @@ namespace HighSchool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> S = new List<Student>
+            {
+                new Student(){LastNameOfStudent = "Doddruesing", FirstNameOfStudent = "Melissa", Grade = 12, GPA = 3.9 },
+                new Student() { LastNameOfStudent = "Meyer", FirstNameOfStudent = "Aimee", Grade = 9, GPA = 3.5 },
+                new Student() { LastNameOfStudent = "Doddruesing", FirstNameOfStudent = "Grace", Grade = 11, GPA = 3.5 },
+                new Student() { LastNameOfStudent = "Doddruesing", FirstNameOfStudent = "Lily", Grade = 12, GPA = 3.75 },
+                new Student() { LastNameOfStudent = "Meyer", FirstNameOfStudent = "Caroline", Grade = 5, GPA = 2 },
+                new Student() { LastNameOfStudent = "Meyer", FirstNameOfStudent = "Nisha", Grade = 6, GPA = 2.9 },
+                new Student() { LastNameOfStudent = "Walser", FirstNameOfStudent = "David", Grade = 12, GPA = 3.2 },
+                new Student() { LastNameOfStudent = "Ricker", FirstNameOfStudent = "Marilyn", Grade = 12, GPA = 3.0 },
+                new Student() { LastNameOfStudent = "Beard", FirstNameOfStudent = "Julie", Grade = 10, GPA = 3.4 },
+                new Student() { LastNameOfStudent = "Beard", FirstNameOfStudent = "Dan", Grade = 10, GPA = 2.9 },
+            };
+
+            var SmartestStudent = S.Where(p => p.Grade >= 9 && p.GPA >= 3.0);
+            //Console.WriteLine(SmartestStudent);
+            SmartestStudent = SmartestStudent.OrderByDescending(p => p.Grade).ThenByDescending(p => p.GPA).ThenBy(p => p.LastNameOfStudent).ThenBy(p => p.FirstNameOfStudent);
+            
+
+
+            foreach (var item in SmartestStudent)
+            {
+                Console.WriteLine("{0} {1} {2} {3}", item.Grade, item.GPA, item.LastNameOfStudent, item.FirstNameOfStudent
+                    );
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ HighSchool/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HighSchool
+{
+    class Student
+    {
+        public string FirstNameOfStudent { get; set; }
+        public string LastNameOfStudent { get; set; }
+        public int Grade { get; set; }
+        public double GPA { get; set; }
+
+
+
+
+    }
+}
